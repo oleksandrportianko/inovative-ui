d3688b350102e71bc7938098fbddacc41e69a899
Oleksandr <portianko123@gmail.com>
2022-11-21 12:39:43 +0200

    update alert component

--- src/components/alert/alert.component.tsx
+++ src/components/alert/alert.component.tsx
@@ -8,11 +8,19 @@ type AlertProps = {
     variant?: 'success' | 'error',
     position?: 'bottom-right' | 'top-right' | 'bottom-left' | 'top-left',
     close?: 'auto' | 'manually',
+    closetime?: number, 
     content: string,
     onClose: () => any;
 }
 
-export const Alert: FC<AlertProps> = ({ variant = 'success', position = 'bottom-right', close = 'manually', content, onClose }) => {
+export const Alert: FC<AlertProps> = ({ variant = 'success', position = 'bottom-right', close = 'manually', closetime = 3000, content, onClose }) => {
+
+    if (close === 'auto') {
+        setTimeout(() => {
+            onClose()
+        }, closetime)
+    }
+
     return (
         <div className={`alert-container alert-${variant} alert-${position}`}>
             {content}
--- src/pages/elements/elements.page.tsx
+++ src/pages/elements/elements.page.tsx
@@ -4,14 +4,14 @@ import { Alert } from "../../components/alert/alert.component"
 
 export const Elements = () => {
     const [alert, setAlert] = useState<boolean>(true)
-    
+
     const closeAlert = () => {
         setAlert(false)
     }
 
     return (
         <div>
-            { alert && <Alert onClose={closeAlert} variant="success" position="bottom-right" content="Text text for text texxt" /> }
+            { alert && <Alert close="manually" onClose={closeAlert} variant="success" position="bottom-right" content="Text text for text texxt" /> }
         </div>
     )
 }
